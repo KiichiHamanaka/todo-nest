c2afaf881aeb340b798baaf1e083660a049fb5a8
agoetc <tkgw666private@gmail.com>
2021-06-02 22:45:56 +0900

    prismaの追加, sqliteを使用するように

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,6 +1,7 @@
 import {Controller, Get, Param, Req} from '@nestjs/common';
 import {TodoOutputType} from "../interface";
 import table from '../data'
+import { PrismaClient } from '@prisma/client';
 
 @Controller('todo')
 export class TodoController {
@@ -11,8 +12,14 @@ export class TodoController {
     }
 
     @Get(':id')
-    getTodo(@Param() params): TodoOutputType { //TODO: paramsの型を指定する
-        return table.find(row => row.id === parseInt(params.id))
+    async getTodo(@Param() params): Promise<any> { //TODO: paramsの型を指定する
+        const prisma = new PrismaClient()
+        const todos = await prisma.todo.findFirst({
+            where: {
+                id: Number(params.id)
+            }
+        })
+        return todos
     }
 
 
