f2209e5c3be7fe0f6167c1306455a06ff881c5e8
agoetc <tkgw666private@gmail.com>
2021-06-02 22:51:46 +0900

    formatted

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,19 +1,17 @@
-import {Controller, Get, Param, Req} from '@nestjs/common';
-import {TodoOutputType} from "../interface";
-import table from '../data'
+import { Controller, Get, Param, Req } from '@nestjs/common';
+import { TodoOutputType } from '../interface';
+import table from '../data';
 
 @Controller('todo')
 export class TodoController {
+  @Get()
+  getHello(): string {
+    return 'homepage';
+  }
 
-    @Get()
-    getHello(): string {
-        return 'homepage'
-    }
-
-    @Get(':id')
-    getTodo(@Param() params): TodoOutputType { //TODO: paramsの型を指定する
-        return table.find(row => row.id === parseInt(params.id))
-    }
-
-
+  @Get(':id')
+  getTodo(@Param() params): TodoOutputType {
+    //TODO: paramsの型を指定する
+    return table.find((row) => row.id === parseInt(params.id));
+  }
 }
