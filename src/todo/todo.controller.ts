bdf1fafa928ba92461ad9c09d9cc4fdb6b89a98c
agoetc <tkgw666private@gmail.com>
2021-06-02 23:03:25 +0900

    返り値型を指定するように（anyにしちゃってた）

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,6 +1,5 @@
-import { Controller, Get, Param, Req } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { TodoOutputType } from '../interface';
-import table from '../data';
 import { PrismaClient } from '@prisma/client';
 
 @Controller('todo')
@@ -11,14 +10,13 @@ export class TodoController {
   }
 
   @Get(':id')
-  async getTodo(@Param() params): Promise<any> {
+  async getTodo(@Param() params): Promise<TodoOutputType> {
     //TODO: paramsの型を指定する
     const prisma = new PrismaClient();
-    const todos = await prisma.todo.findFirst({
+    return await prisma.todo.findFirst({
       where: {
         id: Number(params.id),
       },
     });
-    return todos;
   }
 }
