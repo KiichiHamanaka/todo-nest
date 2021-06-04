7d89b4a7a59bf0ed5f18b71bab4fbd336bdfca7d
agoetc <tkgw666private@gmail.com>
2021-06-05 03:12:10 +0900

    hogehoge

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,12 +1,38 @@
 import { Controller, Get, Param, Req } from '@nestjs/common';
 import { TodoOutputType } from '../interface';
 import table from '../data';
+import axios from 'axios';
+import { zip } from 'rxjs';
 
 @Controller('todo')
 export class TodoController {
   @Get()
-  getHello(): string {
+  async getHello(): Promise<string> {
+    await this.postDataAxios();
     return 'homepage';
+  }
+
+  async postDataAxios() {
+    const headers = {
+      'Content-Type': 'application/json',
+      mode: 'cors',
+      withCredentials: 'true',
+    };
+    await axios
+      .post(
+        'https://techtrain-railway-api.herokuapp.com/users',
+        {
+          name: 'string',
+          email: 'string',
+          password: 'string',
+        },
+        {
+          headers,
+        },
+      )
+      .then((response) => {
+        console.log(response.data);
+      });
   }
 
   @Get(':id')
