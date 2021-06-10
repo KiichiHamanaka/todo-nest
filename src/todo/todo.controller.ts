ad18233b2150e82421a5d9700161ffc4289163f8
agoetc <tkgw666private@gmail.com>
2021-06-10 12:15:51 +0000

    [ci] format

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,19 +1,16 @@
-import {Controller, Get, Param, Req} from '@nestjs/common';
-import {TodoOutputType} from "../interface";
-import table from '../data'
+import { Controller, Get, Param, Req } from '@nestjs/common';
+import { TodoOutputType } from '../interface';
+import table from '../data';
 
 @Controller('todo')
 export class TodoController {
+  @Get()
+  getHello(): string {
+    return 'homepage';
+  }
 
-    @Get()
-    getHello(): string {
-        return 'homepage'
-    }
-
-    @Get(':id')
-    getTodo(@Param('id') id:string): TodoOutputType {
-        return table.find(row => row.id === parseInt(id))
-    }
-
-
+  @Get(':id')
+  getTodo(@Param('id') id: string): TodoOutputType {
+    return table.find((row) => row.id === parseInt(id));
+  }
 }
