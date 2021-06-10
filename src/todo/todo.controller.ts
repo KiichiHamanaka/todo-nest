c32bbce5e8ac3127c22c3c4d76adccc6360b6f73
KiichiHamanaka <b1695205@planet.kanazawa-it.ac.jp>
2021-06-10 22:54:37 +0900

    serviceにfindとupdate関数の共通化メソッドを作成

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,9 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
 import { TodoOutputType } from '../interface';
-import { PrismaClient } from '@prisma/client';
+import { TodoService } from './todo.service';
 
 @Controller('todo')
 export class TodoController {
+  service = new TodoService();
+
   @Get()
   getHello(): string {
     return 'homepage';
@@ -11,43 +13,18 @@ export class TodoController {
 
   @Get(':id')
   async getTodo(@Param('id') id: string): Promise<TodoOutputType> {
-    const prisma = new PrismaClient();
-    return await prisma.todo.findFirst({
-      where: {
-        id: Number(id),
-      },
-    });
+    return this.service.getTodo(id);
   }
 
   @Get(':id/to-close')
   async changeClose(@Param('id') id: string): Promise<TodoOutputType> {
-    const prisma = new PrismaClient();
-    await prisma.todo.update({
-      where: {
-        id: Number(id),
-      },
-      data: { status: 'close' },
-    });
-    return await prisma.todo.findFirst({
-      where: {
-        id: Number(id),
-      },
-    });
+    await this.service.updateStatus(id, 'close');
+    return this.service.getTodo(id);
   }
 
   @Get(':id/to-open')
   async changeOpen(@Param('id') id: string): Promise<TodoOutputType> {
-    const prisma = new PrismaClient();
-    await prisma.todo.update({
-      where: {
-        id: Number(id),
-      },
-      data: { status: 'open' },
-    });
-    return await prisma.todo.findFirst({
-      where: {
-        id: Number(id),
-      },
-    });
+    await this.service.updateStatus(id, 'open');
+    return this.service.getTodo(id);
   }
 }
--- src/todo/todo.service.ts
+++ src/todo/todo.service.ts
@@ -1,4 +1,25 @@
 import { Injectable } from '@nestjs/common';
+import { TodoOutputType } from '../interface';
+import { PrismaClient } from '@prisma/client';
 
 @Injectable()
-export class TodoService {}
+export class TodoService {
+  async getTodo(id: string): Promise<TodoOutputType> {
+    const prisma = new PrismaClient();
+    return await prisma.todo.findFirst({
+      where: {
+        id: Number(id),
+      },
+    });
+  }
+
+  async updateStatus(id: string, status: string): Promise<TodoOutputType> {
+    const prisma = new PrismaClient();
+    return await prisma.todo.update({
+      where: {
+        id: Number(id),
+      },
+      data: { status: status },
+    });
+  }
+}
